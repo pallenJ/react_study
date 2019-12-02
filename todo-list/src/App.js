99b6c66e0598aacacabd4007a548811d225012c6
pallenJ <pallennellap17@gmail.com>
2019-12-02 15:16:36 +0900

    form 생성

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { TodoListTemplate } from './component/TodoListTemplate';
+import { Form } from './component/Form';
 
 function App() {
   return (
-    <TodoListTemplate>
+    <TodoListTemplate form = {<Form></Form>}>
       기본 템플릿
     </TodoListTemplate>
 
---
+++ todo-list/src/component/Form.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './Form.css';
+
+export const Form = ({value, onChange, onCreate, onKeyPress}) => {//value: 입력내용, onCreate : 버튼클릭시 이벤트 , onChange : 내용이 변경시 실행함수 onKeyPress : input안에서 키보드로 입력시 이벤트
+  return (
+    <div className="form">
+      <input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
+      <div className="create-button" onClick={onCreate}>
+        추가
+      </div>
+    </div>
+  );
+};
