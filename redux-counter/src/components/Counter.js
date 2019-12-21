fd6a000a564558c02cfb8f282122acf3e2a9888c
pallenJ <pallennellap17@gmail.com>
2019-12-21 14:05:08 +0900

    Counter 베이스 완성

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import './Counter.css';
 
 export const Counter = ({number,color,onIncrement,onDecrement,onSetColor}) => {//현재숫자, 색깔, 증가/감소이벤트, 색깔 지정
@@ -6,8 +7,32 @@ export const Counter = ({number,color,onIncrement,onDecrement,onSetColor}) => {/
         <div
         className = "Counter"
         onClick = {onIncrement}
+        onContextMenu = {
+            (e) => {
+                e.preventDefault();
+                onDecrement();
+            }
+        }
+        onDoubleClick = {onSetColor}
+        style = {{backgroundColor:color}}
         >
-
+            {number}
         </div>
     )
+}
+
+Counter.propTypes = {
+    number : PropTypes.number,
+    color  : PropTypes.string,
+    onIncrement : PropTypes.func,
+    onDecrement : PropTypes.func,
+    onSetColor  : PropTypes.func
+}
+
+Counter.defaultProps = {
+    number : 0,
+    color  : 'black',
+    onIncrement : () =>console.warn('onIncreament not defined'),
+    onDecrement : () =>console.warn('onDecreament not defined'),
+    onSetColor  : () =>console.warn('onSetColor not defined')
 }
