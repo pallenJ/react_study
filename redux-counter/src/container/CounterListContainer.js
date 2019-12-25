f185f28f7085f660f787cdd520f7d8a962693f00
pallenJ <pallennellap17@gmail.com>
2019-12-25 14:56:29 +0900

    CounterListContainer.js 생성

---
+++ redux-counter/src/container/CounterListContainer.js
@@ -0,0 +1,15 @@
+import CounterList from '../components/CounterList';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+import { getRandomColor } from '../utils';
+
+
+const CounterListContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CounterListContainer
