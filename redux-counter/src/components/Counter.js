94e47e8eb11ed08da4408498801e016358924414
pallenJ <pallennellap17@gmail.com>
2019-12-25 15:36:29 +0900

    다른 에러는 없으나 클릭시 증가하지 않고 타입이 넘버가 아닌 에러 발생

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -1,42 +1,40 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Counter.css';
 
-export const Counter = ({number,color,index,onIncrement,onDecrement,onSetColor}) => {//현재숫자, 색깔, 증가/감소이벤트, 색깔 지정
-   // alert(color)
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
     return (
-        <div
-        className = "Counter"
-        onClick = {onIncrement(index)}
-        onContextMenu = {
-            (e) => {
-                e.preventDefault();
-                onDecrement(index);
-            }
-        }
-        onDoubleClick = {onSetColor(index)}
-        style = {{backgroundColor:color}}
-        >
-            {number}
+        <div 
+            className="Counter" 
+            onClick={() => onIncrement(index)} 
+            onContextMenu={
+                (e) => { 
+                    e.preventDefault(); 
+                    onDecrement(index);
+                }
+            } 
+            onDoubleClick={() => onSetColor(index)}
+            style={{backgroundColor: color}}>
+                {number}
         </div>
-    )
-}
+    );
+};
 
 Counter.propTypes = {
-    index  : PropTypes.number,
-    number : PropTypes.number,
-    color  : PropTypes.string,
-    onIncrement : PropTypes.func,
-    onDecrement : PropTypes.func,
-    onSetColor  : PropTypes.func
-}
+    index: PropTypes.number,
+    number: PropTypes.number,
+    color: PropTypes.string,
+    onIncrement: PropTypes.func,
+    onDecrement: PropTypes.func,
+    onSetColor: PropTypes.func
+};
 
 Counter.defaultProps = {
-    index : 0,
-    number : 0,
-    color  : 'black',
-    onIncrement : () =>console.warn('onIncreament not defined'),
-    onDecrement : () =>console.warn('onDecreament not defined'),
-    onSetColor  : () =>console.warn('onSetColor not defined')
-}
+    index: 0,
+    number: 0,
+    color: 'black',
+    onIncrement: () => console.warn('onIncrement not defined'),
+    onDecrement: () => console.warn('onDecrement not defined'),
+    onSetColor: () => console.warn('onSetColor not defined')
+};
 export default Counter;
