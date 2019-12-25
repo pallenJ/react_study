767bba99e5a981f9951553441be90fef67eea699
pallenJ <pallennellap17@gmail.com>
2019-12-25 13:45:49 +0900

    멀티 카운터 시작

--- redux-counter/src/actions/index.js
+++ redux-counter/src/actions/index.js
@@ -4,17 +4,29 @@ import * as types from './ActionTypes';
     action 객체를 만드는 액션 생성자들을 선언
     () => ({}) 는 function() { return { } } 와 동일함.(기억할것.)
 */
-export const increment = () => ({
-    type : types.INCREMENT
+
+
+export const create = () => ({
+    type : types.CREATE
 });
 
-export const decrement = () => ({
-    type : types.DECREMENT
+export const remove = () => ({
+    type : types.REMOVE
+});
+//기존의 함수에 index추가. 어느 component의 이벤트인지 알기위함.
+export const increment = (index) => ({
+    type : types.INCREMENT,
+    index
+});
+
+export const decrement = (index) => ({
+    type : types.DECREMENT,
+    index
 });
 
 export const setColor  = (color) => ({
     type : types.SET_COLOR,
+    index,
     color : color
-});//유일하게 파라미터를 가짐. 변경할 색상을 받아야하기 때문.
-
+});
  
