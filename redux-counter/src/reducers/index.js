b79bafc941c096effacaa08fe0cf9433d33f2b18
pallenJ <pallennellap17@gmail.com>
2019-12-25 14:12:43 +0900

    색상변경 및 숫자증가감소 리듀서 추가

--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -16,6 +16,8 @@ function counter(state = initialState, action) {
     const {counters} = state;
 
     switch (action.type) {
+        
+        // 카운터를 새로 추가
         case types.CREATE:
             
             return{
@@ -26,12 +28,45 @@ function counter(state = initialState, action) {
                     }
                 ]
             };
+        // slice 를 이용하여 맨 마지막 카운터 제거
         case types.REMOVE:
             
             return{
                 counters : counters.slice(0,counters.length-1)//const로 생성했으므로 
             };
-    
+        // action.index 번째 카운터의 number가 1 커짐
+        case types.INCREMENT:
+            return {counters:[
+                ...counters.slice(0,action.index),
+                {
+                    ...counters[action.index],
+                    number : counters[action.index].number+1
+                },
+                ...counters.slice(action.index+1,counter.length)
+                ]
+            };
+        // action.index 번째 카운터의 number가 1 줄음
+        case types.DECREMENT:
+            return {counters:[
+                ...counters.slice(0,action.index),
+                {
+                    ...counters[action.index],
+                    number : counters[action.index].number-1
+                },
+                ...counters.slice(action.index+1,counter.length)
+                ]
+            };
+        // action.index 번째 카운터의 색상 변경
+        case types.SET_COLOR:
+            return {counters:[
+                ...counters.slice(0,action.index),
+                {
+                    ...counters[action.index],
+                    color : action.color
+                },
+                ...counters.slice(action.index+1,counter.length)
+                ]
+            };
         default:
             return state;
     }
